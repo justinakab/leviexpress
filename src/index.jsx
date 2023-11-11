7bd5d1ec221285cc1009678ea084d3abaccdb252
Justina Kabelková <justina.kabel@gmail.com>
2023-11-11 09:49:27 +0100

    setting up router and creating the reservation page

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,26 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
 import { App } from './components/App';
 import './global.css';
+import { ReservationPage } from './pages/Reservation';
+import { HomePage } from './pages/HomePage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children: [
+      {
+        path: '',
+        element: <HomePage />,
+      },
+      {
+        path: 'reservation',
+        element: <ReservationPage></ReservationPage>,
+      },
+    ],
   },
 ]);
 
 createRoot(document.querySelector('#app')).render(
-  <RouterProvider router={router} />
+  <RouterProvider router={router} />,
 );
